356097426afab29e7fc147fd60f126549204769f
alumno <al217769@alumnos.uacj.mx>
2026-02-09 08:41:52 -0700

    [AGREGA] proyecto vacio en xcode

---
+++ pruebaHolaMundo/pruebaHolaMundo/pruebaHolaMundoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaHolaMundoApp.swift
+//  pruebaHolaMundo
+//
+//  Created by alumno on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct pruebaHolaMundoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
